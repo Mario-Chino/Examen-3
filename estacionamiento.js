3337ad226fa30506d54a51754bcc413d0b99de9b
Mario-Chino <udiproyect@gmail.com>
2024-05-24 09:11:56 -0400

    Create estacionamiento.js

---
+++ estacionamiento.js
@@ -0,0 +1,188 @@
+var caja = document.getElementById('caja'),
+    contador=0;
+
+    function cambio()
+    { if(contador==0)
+        {
+            caja.classList.add('rojo');
+            contador=1;
+            alert("solo si heres discapacitado");
+            
+        }
+        else{caja.classList.remove('rojo');
+            contador=0;
+            alert("lo quieres desocupar");
+            
+        }
+    }
+    caja.addEventListener('click',cambio,true)
+
+var caja2 = document.getElementById('caja2'),
+    contador2=0;
+
+    function cambio2()
+    { if(contador2==0)
+        {
+            caja2.classList.add('rojo2');
+            contador2=1
+            alert("lo quieres ocupar");
+        }
+        else{caja2.classList.remove('rojo2');
+            contador2=0;
+            alert("lo quieres desocupar");
+        }
+    }
+    caja2.addEventListener('click',cambio2,true)
+
+var caja3 = document.getElementById('caja3'),
+    contador3 = 0;
+
+    function cambio3() {
+        if (contador3 == 0) {
+            caja3.classList.add('rojo3');
+            contador3 = 1;
+            alert("lo quieres ocupar");
+        } else {
+            caja3.classList.remove('rojo3');
+            contador3 = 0;
+            alert("lo quieres desocupar");
+        }
+    }
+    
+    caja3.addEventListener('click', cambio3, true);
+
+var caja4 = document.getElementById('caja4'),
+    contador4 = 0;
+
+    function cambio4() {
+        if (contador4 == 0) {
+            caja4.classList.add('rojo4');
+            contador4 = 1;
+            alert("lo quieres ocupar");
+        } else {
+            caja4.classList.remove('rojo4');
+            contador4 = 0;
+            alert("lo quieres desocupar");
+        }
+    }
+    
+    caja4.addEventListener('click', cambio4, true);
+
+var caja5 = document.getElementById('caja5'),
+    contador5 = 0;
+
+    function cambio5() {
+        if (contador5 == 0) {
+            caja5.classList.add('rojo5');
+            contador5 = 1;
+            alert("lo quieres ocupar");
+        } else {
+            caja5.classList.remove('rojo5');
+            contador5 = 0;
+            alert("lo quieres desocupar");
+        }
+    }
+    
+    caja5.addEventListener('click', cambio5, true);
+
+var caja6 = document.getElementById('caja6'),
+    contador6 = 0;
+
+    function cambio6() {
+        if (contador6 == 0) {
+            caja6.classList.add('rojo6');
+            contador6 = 1;
+            alert("solo si heres discapacitado");
+        } else {
+            caja6.classList.remove('rojo6');
+            contador6 = 0;
+            alert("lo quieres desocupar");
+        }
+    }
+    
+    caja6.addEventListener('click', cambio6, true);
+
+var caja7 = document.getElementById('caja7'),
+    contador7 = 0;
+
+    function cambio7() {
+        if (contador7 == 0) {
+            caja7.classList.add('rojo7');
+            contador7 = 1;
+            alert("lo quieres ocupar");
+        } else {
+            caja7.classList.remove('rojo7');
+            contador7 = 0;
+            alert("lo quieres desocupar");
+        }
+    }
+    
+    caja7.addEventListener('click', cambio7, true);
+
+
+var caja8 = document.getElementById('caja8'),
+    contador8 = 0;
+
+    function cambio8() {
+        if (contador8 == 0) {
+            caja8.classList.add('rojo8');
+            contador8 = 1;
+            alert("lo quieres ocupar");
+        } else {
+            caja8.classList.remove('rojo8');
+            contador8 = 0;
+            alert("lo quieres desocupar");
+        }
+    }
+    
+    caja8.addEventListener('click', cambio8, true);
+
+var caja9 = document.getElementById('caja9'),
+    contador9 = 0;
+
+    function cambio9() {
+        if (contador9 == 0) {
+            caja9.classList.add('rojo9');
+            contador9 = 1;
+            alert("lo quieres ocupar");
+        } else {
+            caja9.classList.remove('rojo9');
+            contador9 = 0;
+            alert("lo quieres desocupar");
+        }
+    }
+    
+    caja9.addEventListener('click', cambio9, true);
+
+var caja10 = document.getElementById('caja10'),
+    contador10 = 0;
+
+    function cambio10() {
+        if (contador10 == 0) {
+            caja10.classList.add('rojo10');
+            contador10 = 1;
+            alert("lo quieres ocupar");
+        } else {
+            caja10.classList.remove('rojo10');
+            contador10 = 0;
+            alert("lo quieres desocupar");
+        }
+    }
+    
+    caja10.addEventListener('click', cambio10, true);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
